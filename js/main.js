39b5cfa2672c91158fb2c66d154e5edc95f338e8
trizgb <beatrizgomez92@gmail.com>
2018-12-09 20:05:33 +0100

    finished js part and doing css

--- js/main.js
+++ js/main.js
@@ -6,26 +6,21 @@ const button = document.querySelector('.btn');
 
 function handleClick () {
     let userName = input.value;
-    //console.log(userName)
     fetch(`https://api.github.com/users/${userName}`)
     .then(response => response.json())
     .then (data => {
         const showName = document.querySelector('.show-name');
         let selectedName = data.name;
         selectedName = selectedName.split(" ");
-        console.log(selectedName[0]);
         let partitionName = selectedName[0];
         partitionName = partitionName.split("");
-        console.log(partitionName.length);
-        for (let i = 0; i <= partitionName.length; i++){
-            
-            showName.innerHTML = '<li></li>'
+        let letterList = '';
+        for (let i = 0; i < partitionName.length; i++){  
+            letterList += `<li class="item"> ${partitionName[i]} </li>`;
         }
+        showName.innerHTML = letterList;
     });
 };
 
 button.addEventListener('click', handleClick);
-
-//promesa seleccionar nombre real de la criatura
-//escribimos cada letra en un li de una lista ordenada
 
