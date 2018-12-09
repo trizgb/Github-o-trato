1164d03fb16b57d6ff65f5e2016d4210d2969dc3
trizgb <beatrizgomez92@gmail.com>
2018-12-09 18:50:03 +0100

    doing for

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const input = document.querySelector('.input');
+const button = document.querySelector('.btn');
+
+
+function handleClick () {
+    let userName = input.value;
+    //console.log(userName)
+    fetch(`https://api.github.com/users/${userName}`)
+    .then(response => response.json())
+    .then (data => {
+        const showName = document.querySelector('.show-name');
+        let selectedName = data.name;
+        selectedName = selectedName.split(" ");
+        console.log(selectedName[0]);
+        let partitionName = selectedName[0];
+        partitionName = partitionName.split("");
+        console.log(partitionName.length);
+        for (let i = 0; i <= partitionName.length; i++){
+            
+            showName.innerHTML = '<li></li>'
+        }
+    });
+};
+
+button.addEventListener('click', handleClick);
+
+//promesa seleccionar nombre real de la criatura
+//escribimos cada letra en un li de una lista ordenada
+
